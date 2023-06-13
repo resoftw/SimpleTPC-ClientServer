9a3c11a0193316da2a252c793be9223c79abcf9e
resoftw <robyedrian@gmail.com>
2023-06-13 20:24:57 +0700

    Add project files.

---
+++ Client/Program.cs
@@ -0,0 +1,38 @@
+﻿using SimpleTCP;
+namespace Client
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            SimpleTcpClient client = new SimpleTcpClient();
+            Console.WriteLine("Siap-siap!");
+            Console.ReadKey(true);
+            try
+            {
+                client.Connect("localhost", 1000);
+                client.Delimiter = 0x0A;
+                client.DelimiterDataReceived += Client_DelimiterDataReceived;
+                client.WriteAndGetReply("Hello Bro!\nYES\n");
+                while (true)
+                {
+                    if (Console.KeyAvailable)
+                    {
+                        var c = Console.ReadKey(true);
+                        if (c.Key == ConsoleKey.Escape) break;
+                    }
+                }
+                client.Disconnect();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+
+        private static void Client_DelimiterDataReceived(object? sender, Message e)
+        {
+            Console.WriteLine("|"+e.MessageString+"|");
+        }
+    }
+}
---
+++ Server/Program.cs
@@ -0,0 +1,49 @@
+﻿using SimpleTCP;
+using System.Net.Sockets;
+
+namespace Server
+{
+    internal class Program
+    {
+        static List<TcpClient> tcpClients = new List<TcpClient>();
+        static void Main(string[] args)
+        {
+            SimpleTcpServer server = new SimpleTcpServer().Start(1000);
+            server.ClientConnected += Server_ClientConnected;
+            server.Delimiter = 0x0A;
+            server.DelimiterDataReceived += Server_DelimiterDataReceived;
+            server.ClientDisconnected += Server_ClientDisconnected;
+            Console.WriteLine("Listening...!");
+            while (true)
+            {
+                Console.Write(".");
+                foreach(var c in tcpClients)
+                {
+                    if (c.Connected)
+                    {
+                        c.Client.Send(new byte[] { (byte)'.', (byte)'\n' });
+                    }
+                }
+                Thread.Sleep(1000);
+            }
+        }
+
+        private static void Server_ClientDisconnected(object? sender, TcpClient e)
+        {
+            tcpClients.Remove(e);
+            Console.WriteLine("Client disconnected!");
+        }
+
+        private static void Server_DelimiterDataReceived(object? sender, Message e)
+        {
+            Console.WriteLine("\n|"+e.MessageString+"|");
+            e.ReplyLine("OK");
+        }
+
+        private static void Server_ClientConnected(object? sender, TcpClient e)
+        {
+            tcpClients.Add(e);
+            Console.WriteLine("Client connected {0}", e.Client.RemoteEndPoint);
+        }
+    }
+}
